386b5d67c7dd721854dd15029c24d135d1c442a8
gaozhou <generalgz@163.com>
2023-08-23 21:11:17 +0800

    success run

--- example/testStorm.cc
+++ example/testStorm.cc
@@ -2,18 +2,20 @@
 #include <iostream>
 
 int main() {
-    storm::Logger::ptr logger(new storm::Logger);
-    logger->addAppender(storm::LogAppender::ptr(new storm::StdoutLogAppender));
+    sylar::Logger::ptr logger(new sylar::Logger);
+    logger->addAppender(sylar::LogAppender::ptr(new sylar::StdoutLogAppender));
 
     // logger->debug("Hello, World!");
-    storm::LogEvent::ptr event(new storm::LogEvent(__FILE__, __LINE__, 0, 1, 2, time(0)));
-    std::cout << "event init success" << std::endl;
-    event->getSS() << "hello storm log"; 
-    logger->log(storm::LogLevel::DEBUG, event);
+    sylar::LogEvent::ptr event(new sylar::LogEvent(logger,sylar::LogLevel::DEBUG, 
+    __FILE__, __LINE__, 0, 1, 2, time(0), "wangbing"));
+
     
-    std::string nstr;
-    nstr.append(1, '%');
-    std::cout << nstr << std::endl;
-    std::cout << "Hello storm log" << std::endl;
+    // LogEvent(std::shared_ptr<Logger> logger, LogLevel::Level level
+    //         ,const char* file, int32_t line, uint32_t elapse
+    //         ,uint32_t thread_id, uint32_t fiber_id, uint64_t time
+    //         ,const std::string& thread_name);
+    event->getSS() << "hello storm log"; 
+    logger->log(sylar::LogLevel::DEBUG, event);
+
     return 0;
 }
